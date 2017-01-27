fabe602bb773f09939cdc9811cc0bfb2c6aa4fa2
Kris McGinnes <kmcginnes@me.com>
2017-01-27 15:08:06 -0600

    Made SomeLongRunningService start a new thread

--- src/Example.Bootstrapping/SomeLongRunningService.cs
+++ src/Example.Bootstrapping/SomeLongRunningService.cs
@@ -1,16 +1,43 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Example.Bootstrapping
 {
-    public class SomeLongRunningService : ILongRunningService
+    public class SomeLongRunningService : ILongRunningService, IDisposable
     {
+        private readonly CancellationTokenSource _tokenSource;
+        private readonly Thread _thread;
+
+        public SomeLongRunningService()
+        {
+            _tokenSource = new CancellationTokenSource();
+            _thread = new Thread(Execute);
+        }
+
         public Task Initialize()
         {
             this.Log().Info($"Initializing {nameof(SomeLongRunningService)}");
-            Task.Delay(TimeSpan.FromSeconds(2)).Wait();
+            _thread.Start(_tokenSource.Token);
             this.Log().Info($"Finished initializing {nameof(SomeLongRunningService)}");
             return Task.FromResult(true);
+        }
+
+        public void Execute(object param)
+        {
+            var token = (CancellationToken) param;
+            while (!token.IsCancellationRequested)
+            {
+                this.Log().Debug("Still working...");
+                Thread.Sleep(1000);
+            }
+            this.Log().Debug("Cancelled our work");
+        }
+
+        public void Dispose()
+        {
+            _tokenSource.Cancel();
+            this.Log().Debug("Inside Dispose()");
         }
     }
 }
