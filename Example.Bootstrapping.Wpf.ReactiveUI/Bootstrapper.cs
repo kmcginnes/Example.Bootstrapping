aa0a90c81d6baa29c3ceaeb3a34972abef7aaea4
Kris McGinnes <kmcginnes@me.com>
2017-02-05 20:54:03 -0600

    Add logging and config parser

--- Example.Bootstrapping.Wpf.ReactiveUI/Bootstrapper.cs
+++ Example.Bootstrapping.Wpf.ReactiveUI/Bootstrapper.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Configuration;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 
 namespace Example.Bootstrapping.Wpf.ReactiveUI
@@ -13,6 +16,23 @@ namespace Example.Bootstrapping.Wpf.ReactiveUI
         public static void Main(string[] args)
         {
             FreeConsole();
+
+            var banner = new StringBuilder();
+            banner.AppendLine(@" ______               __          __                                     ");
+            banner.AppendLine(@"|   __ \.-----.-----.|  |_.-----.|  |_.----.---.-.-----.-----.-----.----.");
+            banner.AppendLine(@"|   __ <|  _  |  _  ||   _|__ --||   _|   _|  _  |  _  |  _  |  -__|   _|");
+            banner.AppendLine(@"|______/|_____|_____||____|_____||____|__| |___._|   __|   __|_____|__|  ");
+            banner.AppendLine(@"                                                 |__|  |__|              ");
+            banner.AppendLine(@"    ");
+            var logging = new LoggingOrchestrator();
+            logging.InitializeLogging("Main", banner.ToString());
+
+            GlobalExceptionHandlers.WireUp();
+
+            var environment = new EnvironmentFacade(Assembly.GetExecutingAssembly());
+
+            var appSettings = ConfigurationParser.Parse(args, ConfigurationManager.AppSettings);
+            logging.LogUsefulInformation(environment, appSettings);
 
             var app = new App { ShutdownMode = ShutdownMode.OnLastWindowClose };
             app.InitializeComponent();
---
+++ Example.Bootstrapping.Wpf.ReactiveUI/ConfigurationParser.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace Example.Bootstrapping.Wpf.ReactiveUI
+{
+    public static class ConfigurationParser
+    {
+        private static readonly ILog Logger = typeof(ConfigurationParser).Name.Log();
+
+        public static AppSettings Parse(string[] commandLineArgs, NameValueCollection appSettings)
+        {
+            Logger.Debug("Parsing command line and app settings...");
+            var dataDirectory = GetDataDirectory(appSettings);
+            var defaultVolume = GetDefaultVolume(appSettings);
+
+            var settings = new AppSettings
+            {
+                DataDirectory = dataDirectory,
+                DefaultVolume = defaultVolume,
+            };
+
+            return settings;
+        }
+
+        public static string GetDataDirectory(NameValueCollection appSettings)
+        {
+            var defaultPath = @"C:\Temp\";
+            var configuredPath = appSettings["DataDirectory"];
+
+            if (String.IsNullOrWhiteSpace(configuredPath))
+            {
+                Logger.Info($"AppSettings.DataDirectory was not defined. Using the default value {defaultPath}");
+                return defaultPath;
+            }
+
+            if (!configuredPath.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                configuredPath = configuredPath + Path.DirectorySeparatorChar;
+            }
+
+            return configuredPath;
+        }
+
+        public static int GetDefaultVolume(NameValueCollection appSettings)
+        {
+            const int defaultVolume = 100;
+            const int maxVolume = 200;
+            const int minVolume = 0;
+            var configuredVolume = appSettings["DefaultPlaybackVolume"];
+
+            if (String.IsNullOrWhiteSpace(configuredVolume))
+            {
+                Logger.Info($"AppSettings.DefaultPlaybackVolume was not defined. Using the default value {defaultVolume}");
+                return defaultVolume;
+            }
+
+            int converted;
+
+            if (!Int32.TryParse(configuredVolume, out converted))
+            {
+                Logger.Info($"AppSettings.DefaultPlaybackVolume was not an integer value ({configuredVolume}). Using the default value {defaultVolume}");
+                return defaultVolume;
+            }
+
+            if (converted > maxVolume)
+            {
+                Logger.Info($"AppSettings.DefaultPlaybackVolume was higher than the max value ({configuredVolume}). Using the max value {maxVolume}");
+                return maxVolume;
+            }
+
+            if (converted < minVolume)
+            {
+                Logger.Info($"AppSettings.DefaultPlaybackVolume was lower than the min value ({configuredVolume}). Using the min value {minVolume}");
+                return minVolume;
+            }
+
+            return converted;
+        }
+    }
+}
