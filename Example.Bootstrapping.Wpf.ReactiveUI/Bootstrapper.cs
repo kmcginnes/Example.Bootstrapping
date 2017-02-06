7a9d2cee637301dae4dfb36c7e44ceab16fcb205
Kris McGinnes <kmcginnes@me.com>
2017-02-05 20:07:02 -0600

    Gain control of startup process

---
+++ Example.Bootstrapping.Wpf.ReactiveUI/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace Example.Bootstrapping.Wpf.ReactiveUI
+{
+    public class Bootstrapper
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool FreeConsole();
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            FreeConsole();
+
+            var app = new App { ShutdownMode = ShutdownMode.OnLastWindowClose };
+            app.InitializeComponent();
+
+            var window = new MainWindow();
+
+            window.Show();
+            app.Run();
+        }
+    }
+}
