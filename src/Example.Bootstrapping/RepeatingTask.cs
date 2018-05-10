bb058b69435a2627c7c9e9de3a3b177973124e2b
Kris McGinnes <kmcginnes@me.com>
2018-05-10 11:33:48 -0500

    Add RepeatingTask

---
+++ src/Example.Bootstrapping/RepeatingTask.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example.Bootstrapping
+{
+    public class RepeatingTask
+    {
+        private readonly TimeSpan _interval;
+        private readonly Func<CancellationToken, Task> _task;
+        private readonly CancellationTokenSource _cancellationTokenSource;
+        private readonly Timer _timer;
+
+        public RepeatingTask(TimeSpan interval, Func<CancellationToken, Task> task)
+        {
+            _interval = interval;
+            _task = task;
+            _cancellationTokenSource = new CancellationTokenSource();
+            _timer = new Timer(OnTimerCallbackAsync, null, Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private async void OnTimerCallbackAsync(object state)
+        {
+            try
+            {
+                StopTimer();
+                this.Log().Debug("Calling repeating task...");
+                await _task(_cancellationTokenSource.Token);
+                ResumeTimer();
+            }
+            catch (TaskCanceledException)
+            {
+                // Don't care about this guy
+            }
+            catch (Exception exception)
+            {
+                this.Log().Error(exception, $"Uncaught exception executing repeating task.");
+            }
+        }
+
+        private void StopTimer()
+        {
+            this.Log().Debug("Setting timer to paused...");
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private void ResumeTimer()
+        {
+            this.Log().Debug($"Resuming timer with interval {_interval}...");
+            _timer.Change((int)_interval.TotalMilliseconds, (int)_interval.TotalMilliseconds);
+        }
+
+        public void Start()
+        {
+            this.Log().Debug($"Starting timer with interval {_interval}...");
+            _timer.Change(0, (int)_interval.TotalMilliseconds);
+        }
+
+        public void Stop()
+        {
+            StopTimer();
+            this.Log().Debug("Cancelling the cancellation token source...");
+            _cancellationTokenSource.Cancel();
+        }
+    }
+}
--- src/Example.Bootstrapping/SomeLongRunningService.cs
+++ src/Example.Bootstrapping/SomeLongRunningService.cs
@@ -7,37 +7,42 @@ namespace Example.Bootstrapping
     public class SomeLongRunningService : ILongRunningService, IDisposable
     {
         private readonly CancellationTokenSource _tokenSource;
-        private readonly Thread _thread;
+        private readonly RepeatingTask _repeatingTask;
 
         public SomeLongRunningService()
         {
             _tokenSource = new CancellationTokenSource();
-            _thread = new Thread(Execute);
+            _repeatingTask = new RepeatingTask(TimeSpan.FromSeconds(5), ExecuteAsync);
         }
 
         public Task Initialize()
         {
             this.Log().Info($"Initializing {nameof(SomeLongRunningService)}");
-            //_thread.Start(_tokenSource.Token);
+            _repeatingTask.Start();
             this.Log().Info($"Finished initializing {nameof(SomeLongRunningService)}");
             return Task.FromResult(true);
         }
 
-        public void Execute(object param)
+        public async Task ExecuteAsync(CancellationToken cancellationToken)
         {
-            var token = (CancellationToken) param;
-            while (!token.IsCancellationRequested)
-            {
-                this.Log().Debug("Still working...");
-                Thread.Sleep(1000);
-            }
-            this.Log().Debug("Cancelled our work");
+            this.Log().Info($"Working on something...");
+            await Task.Delay(TimeSpan.FromMilliseconds(500), cancellationToken);
+            this.Log().Info($"Still Working on something...");
+            await Task.Delay(TimeSpan.FromMilliseconds(500), cancellationToken);
+            this.Log().Info($"Still Working on something...");
+            await Task.Delay(TimeSpan.FromMilliseconds(500), cancellationToken);
+            this.Log().Info($"Still Working on something...");
+            await Task.Delay(TimeSpan.FromMilliseconds(500), cancellationToken);
+
+            this.Log().Info($"Done working on something.");
         }
 
         public void Dispose()
         {
-            _tokenSource.Cancel();
             this.Log().Debug("Inside Dispose()");
+            _tokenSource.Cancel();
+            this.Log().Debug("Stopping the repeating task...");
+            _repeatingTask.Stop();
         }
     }
 }
