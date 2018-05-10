cd8d315d6032614be270e18e6c9a09319923e920
Kris McGinnes <kmcginnes@me.com>
2018-05-10 11:56:32 -0500

    Create QuitTopShelfServiceCommandProcessor

---
+++ src/Example.Bootstrapping.TopShelf/QuitTopShelfServiceCommandProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Topshelf;
+
+namespace Example.Bootstrapping.TopShelf
+{
+    class QuitTopShelfServiceCommandProcessor : IConsoleCommandProcessor
+    {
+        private readonly HostControl _hostControl;
+
+        public char InputCharacter => 'q';
+        public string LongName => "Quit app";
+
+        public QuitTopShelfServiceCommandProcessor(HostControl hostControl)
+        {
+            _hostControl = hostControl;
+        }
+
+        public Task Execute()
+        {
+            this.Log().Info("Stopping TopShelf host control...");
+            _hostControl.Stop();
+            return Task.FromResult(true);
+        }
+    }
+}
